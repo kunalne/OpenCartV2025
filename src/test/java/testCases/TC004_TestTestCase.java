43ec74ac3579d9e6634b5a4b017cabcb6ca24aef
Kunal <negikunal0325@gmail.com>
2025-06-08 13:31:21 +0530

    New changes on 8 June

---
+++ src/test/java/testCases/TC004_TestTestCase.java
@@ -0,0 +1,22 @@
+package testCases;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import PageObjects.HomePage;
+import PageObjects.LoginPage;
+import PageObjects.MyAccountPage;
+import PageObjects.RegisterAccountPage;
+import utilities.DataProviders;
+
+public class TC004_TestTestCase extends BaseTest {
+	
+	//Implement new test case to add this in git hub
+}
